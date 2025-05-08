2a292fb3b0c6a7f5e0122a3d79f931307c905361
denleb <denis.lebold@fh-dortmund.de>
2025-05-08 16:25:27 +0000

    Corrected layer_parameters

--- test/test_utils/layer_parameters.py
+++ test/test_utils/layer_parameters.py
@@ -386,7 +386,7 @@ class LayerParameters(object):
         if (self.iact_size_x == 64) :
             self.direct_cycling = 1
             self.iact_x_lines = layer.kernel_size[1] + self.iact_size_y - 1
-            self.needed_standing_cycles = self.used_channels
+            self.needed_standing_cycles = ((self.used_channels + 1) // 2) * self.needed_Iact_writes
         else :
             self.needed_standing_cycles = max(self.needed_standing_cycles,4)
  
