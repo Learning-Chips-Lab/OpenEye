00ce97efee439a5eb6a7dfd811819f53718451b6
denleb <denis.lebold@fh-dortmund.de>
2025-05-16 09:42:38 +0000

    Fixed Continues Loop

--- hdl/router_iact.v
+++ hdl/router_iact.v
@@ -194,12 +194,12 @@ module router_iact #(
 
   assign r00 = (2'd0 == router_mode_i[5:4]) ? (router_mode_i[0] ? ready_dst_port_0 : 1) : ready_dst_port_0;
   assign r10 = (2'd1 == router_mode_i[5:4]) ? (router_mode_i[0] ? ready_dst_port_0 : 1) : (2'd0 == router_mode_i[5:4]) ? ready_dst_port_0 : 1;
-  assign r20 = (2'd2 == router_mode_i[5:4]) ? (router_mode_i[0] ? ready_dst_port_0 : 1) : (2'd0 == router_mode_i[5:4]) ? ready_dst_port_0 : 1;
+  assign r20 = (2'd2 == router_mode_i[5:4]) ? (router_mode_i[0] ? ready_dst_port_0 : 1) : 1;
   assign r30 = (2'd3 == router_mode_i[5:4]) ? (router_mode_i[0] ? ready_dst_port_0 : 1) : (2'd0 == router_mode_i[5:4]) ? ready_dst_port_0 : 1;
 
   assign r01 = (2'd0 == router_mode_i[5:4]) ? (router_mode_i[1] ? ready_dst_port_1 : 1) : (2'd1 == router_mode_i[5:4]) ? ready_dst_port_1 : 1;
   assign r11 = (2'd1 == router_mode_i[5:4]) ? (router_mode_i[1] ? 0 : 0) : ready_dst_port_1;
-  assign r21 = (2'd2 == router_mode_i[5:4]) ? (router_mode_i[1] ? ready_dst_port_1 : 1) : (2'd1 == router_mode_i[5:4]) ? ready_dst_port_1 : 1;
+  assign r21 = (2'd2 == router_mode_i[5:4]) ? (router_mode_i[1] ? ready_dst_port_1 : 1) : 1;
   assign r31 = (2'd3 == router_mode_i[5:4]) ? (router_mode_i[1] ? ready_dst_port_1 : 1) : (2'd1 == router_mode_i[5:4]) ? ready_dst_port_1 : 1;
 
   assign r02 = (2'd0 == router_mode_i[5:4]) ? (router_mode_i[2] ? ready_dst_port_2 : 1) : (2'd2 == router_mode_i[5:4]) ? ready_dst_port_2 : 1;
@@ -209,6 +209,6 @@ module router_iact #(
 
   assign r03 = (2'd0 == router_mode_i[5:4]) ? (router_mode_i[3] ? ready_dst_port_3 : 1) : (2'd3 == router_mode_i[5:4]) ? ready_dst_port_3 : 1;
   assign r13 = (2'd1 == router_mode_i[5:4]) ? (router_mode_i[3] ? ready_dst_port_3 : 1) :  (2'd3 == router_mode_i[5:4]) ? ready_dst_port_3 : 1;
-  assign r23 = (2'd2 == router_mode_i[5:4]) ? (router_mode_i[3] ? 0 : 1) : (2'd3 == router_mode_i[5:4]) ? ready_dst_port_3 : 1;
+  assign r23 = (2'd2 == router_mode_i[5:4]) ? (router_mode_i[3] ? 0 : 1) : 1;
   assign r33 = (2'd3 == router_mode_i[5:4]) ? (router_mode_i[3] ? 0 : 0) : ready_dst_port_3;
 endmodule
