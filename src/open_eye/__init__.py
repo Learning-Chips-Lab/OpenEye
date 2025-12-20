6bc59162e73a8bf391ed41f3309c519181b7850f
Hendrik Woehrle <hendrik.woehrle@dfki.de>
2025-12-20 12:35:48 +0100

    made path aqbsolute and added path for open_eye package

--- src/open_eye/__init__.py
+++ src/open_eye/__init__.py
@@ -4,5 +4,6 @@ import sys
 directory = (os.path.abspath(os.getcwd()))
 sys.path.extend([directory, os.path.dirname(os.path.realpath(__file__))])
 
-test_dir = os.path.join(os.path.abspath(os.path.dirname(__file__)), os.pardir, os.pardir, "test")
-hdl_dir = os.path.join(os.path.abspath(os.path.dirname(__file__)), os.pardir, os.pardir, "hdl")
+test_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, os.pardir, "test"))
+hdl_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, os.pardir, "hdl"))
+open_eye_dir = os.path.abspath(os.path.dirname(__file__))
