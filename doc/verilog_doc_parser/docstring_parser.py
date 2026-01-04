357bc50c42d944298ca61f76e167836738b43c0f
Hendrik Woehrle <hendrik.woehrle@dfki.de>
2026-01-04 11:41:52 +0100

    fixed docstrin parser and updated test

--- doc/verilog_doc_parser/docstring_parser.py
+++ doc/verilog_doc_parser/docstring_parser.py
@@ -4,7 +4,7 @@
 # For more details, see the LICENSE file in the root directory of this project.
 
 
-from pyparsing import Suppress, Word, alphanums, OneOrMore, Group, LineEnd, SkipTo, restOfLine, Optional, White
+from pyparsing import Suppress, Word, alphanums, OneOrMore, ZeroOrMore, Group, LineEnd, SkipTo, restOfLine, Optional, White, Regex, NotAny
 
 
 class ModuleDocumentation:
@@ -31,7 +31,9 @@ def parse_documentation(text):
     single_line_comment = Suppress("///") | Suppress("//!")
     
     # Define content elements
-    word = Word(alphanums + "_" + "." + "-" + "`")
+    word = Word(alphanums + "_.-`")
+    # Port/parameter names: alphanums, underscore, brackets, special chars (but NOT colon/hyphen delimiters)
+    port_param_name = Word(alphanums + "_`[]$/%,*+()")
     description_line = SkipTo(LineEnd())
 
     # brief description
@@ -39,46 +41,62 @@ def parse_documentation(text):
         single_line_comment + word("brief") + newline
     )
 
+    # Empty documentation line: just /// or //! with optional whitespace, but NOT followed by text
+    # This ensures "/// Ports:" doesn't match as an empty line
     empty_documentation_line = single_line_comment + Optional(whitespace) + newline
-    empty_documentation_line.debug = True
-    
+
     # Define module name
     module_name = (
-        single_line_comment + "Module:" + 
+        single_line_comment + "Module:" +
         word("name") + newline
     )
-    
+
     # Define description
     description = Group(
         OneOrMore(
             (single_line_comment + description_line + newline),
             stopOn= single_line_comment + "Parameters:" | single_line_comment + "Ports:"
         )
-    )("description")    
-    
+    )("description")
+
     # Define parameters and ports
+    # Port/param items MUST have leading whitespace (to distinguish from section headers)
+    # followed by a name, then optional whitespace, then : or -, then description
     param_port_item = Group(
-        word("name") + (colon | minus) + restOfLine("description")
+        whitespace + port_param_name("name") + Optional(whitespace) + (colon | minus) + restOfLine("description")
     )
+
+    # A port/param line is either:
+    # - An empty comment line (just /// or //!) - try this first
+    # - A comment followed by whitespace and a param_port_item
+    #   (section headers like "Ports:" don't have leading whitespace so won't match)
+    # Order matters: try empty line first because it's simpler and less likely to partially consume
+    param_port_line = empty_documentation_line | (single_line_comment + param_port_item + newline)
+
+    # Parameters: collect lines until we hit "Ports:"
+    # Since param_port_line won't match "Ports:" (no leading whitespace),
+    # ZeroOrMore will naturally stop there
     parameters = Group(
-        Suppress(single_line_comment + "Parameters:") +
-        OneOrMore((single_line_comment + param_port_item + newline) | empty_documentation_line, 
-                  stopOn = single_line_comment + "Ports:")
+        Suppress(single_line_comment + "Parameters:" + newline) +
+        ZeroOrMore(param_port_line)
     )("parameters")
-    
+
+    # Ports: collect port lines until we hit another section header or end of comment block
+    # Since param_port_line won't match section headers (they lack leading whitespace),
+    # ZeroOrMore will naturally stop
     ports = Group(
-        Suppress(single_line_comment + "Ports:") +
-        OneOrMore((single_line_comment + param_port_item + newline))
+        Suppress(single_line_comment + "Ports:" + newline) +
+        ZeroOrMore(param_port_line)
     )("ports")
     
     # Define the entire documentation block
+    # The structure is: [anything] module_name description [parameters] [ports]
     doc_block = (
         SkipTo(module_name) +
         module_name +
         description +
         Optional(parameters) +
-        SkipTo(ports) + 
-        Optional(ports)
+        Optional(SkipTo(ports) + ports)
     )
     
     # Parse the text
